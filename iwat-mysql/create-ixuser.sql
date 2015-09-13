beae6e32217022e8431cc1896869d3a90df21221
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2015-09-13 14:58:10 +0200

    kaluzki/iwat-mysql: fix image
    Add data volume.
    Move content of the 'create-ixuser.sql' script to the RUN instruction.

--- iwat-mysql/create-ixuser.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER 'ixuser'@'localhost' IDENTIFIED BY 'ixuser';
-GRANT ALL PRIVILEGES ON * . * TO 'ixuser'@'localhost' IDENTIFIED BY 'ixuser' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON  `ixuser\_%` . * TO  'ixuser'@'localhost';
-FLUSH PRIVILEGES;
